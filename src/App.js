5430435269318814e8ce6e6d3833a64afbc49bea
tstike44 <tstike44@hotmail.com>
2019-05-09 23:17:03 -0700

    getting files set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import Wrapper from './components/Wrapper';
+// import Wrapper from './components/Wrapper';
+import characters from './images.json';
 
 class App extends React.Component() {
     state = {
-        pictures
+       characters
     };
 }
 
