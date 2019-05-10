f9788f4026d3a7a39caa433e0925fd6518985525
tstike44 <tstike44@hotmail.com>
2019-05-09 23:23:25 -0700

    yikes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 // import Wrapper from './components/Wrapper';
-import characters from './images.json';
+// import characters from './images.json';
 
-class App extends React.Component() {
-    state = {
-       characters
-    };
-}
+// class App extends React.Component() {
+//     state = {
+//        characters
+//     };
+// }
 
-export default App;
+// export default App;
